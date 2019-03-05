e1d774452af1be4c7cc25129deee4844e0fe12e8
shenglk <shengliukang@hotmail.com>
2019-03-05 17:57:42 +0800

    二分法demo

--- dichotomy_search_demo.py
+++ dichotomy_search_demo.py
@@ -4,15 +4,12 @@ def search(L, e):
 
     def dSearch(L, e, low, high):
         if high == low:
-            print(L[high] == e)
             return L[high] == e
         mid = (high + low) // 2
         if L[mid] == e:
-            print("True")
             return True
         elif L[mid] > e:
             if low == mid:
-                print("False")
                 return False
             else:
                 return dSearch(L, e, low, mid - 1)
@@ -20,10 +17,9 @@ def search(L, e):
             return dSearch(L, e, mid + 1, high)
 
     if len(L) == 0:
-        print("False")
         return False
     else:
         return dSearch(L, e, 0, len(L) - 1)
 
 
-search([1, 2, 3, 4, 5, 8, 9, 10], 5)
+print(search([1, 2, 3, 4, 5, 8, 9, 10], 5))
