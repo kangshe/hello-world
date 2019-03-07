159ba4f8662578f81072207310c12d974b26e166
shenglk <shengliukang@hotmail.com>
2019-03-07 18:02:04 +0800

    # add：0/1背包问题最优解之“暴力解决法（穷举）”

--- greedy_func_demo.py
+++ greedy_func_demo.py
@@ -29,6 +29,7 @@ def density(item):
     return item.getValue() / item.getWeight()
 
 
+# 0/1背包问题之“贪婪算法”
 def greedy(items, maxWeight, keyFunction):
     """假设Items是列表，maxWeight>=0
        keyFunctions将物品元素映射为数值"""
@@ -70,4 +71,58 @@ def testGreedys(maxWeight=20):
     testGreedy(items, maxWeight, density)
 
 
-testGreedys()
+# testGreedys()
+
+def getBinaryRep(n, numDigits):
+    """假设n和numDigits为非负整数
+       返回一个长度为numDigits的字符串，为n的二进制表示"""
+    result = ''
+    while n > 0:
+        result = str(n % 2) + result
+        n = n // 2
+    if len(result) > numDigits:
+        raise ValueError('not enough digits')
+    for i in range(numDigits - len(result)):
+        result = '0' + result
+    return result
+
+
+def genPowerset(L):
+    """假设L是列表
+       返回一个列表，包含L中元素所有可能的集合。例如，如果L=[1, 2]，则返回的列表包含元素[]、[1]、[2]和[1,2]"""
+    powerset = []
+    for i in range(0, 2 ** len(L)):
+        binStr = getBinaryRep(i, len(L))
+        subset = []
+        for j in range(len(L)):
+            if binStr[j] == '1':
+                subset.append(L[j])
+        powerset.append(subset)
+    return powerset
+
+
+# add：0/1背包问题最优解之“暴力解决法（穷举）”
+def chooseBest(pset, maxWeight):
+    bestSet, bestVal = None, 0.0
+    for items in pset:
+        itemsVal = 0.0
+        itemsWeight = 0.0
+        for item in items:
+            itemsVal += item.getValue()
+            itemsWeight += item.getWeight()
+        if itemsWeight <= maxWeight and itemsVal > bestVal:
+            bestVal = itemsVal
+            bestSet = items
+    return (bestSet, bestVal)
+
+
+def testBest(maxWeight=20):
+    items = buildItems()
+    pset = genPowerset(items)
+    taken, val = chooseBest(pset, maxWeight)
+    print('Total value of items taken is', val)
+    for item in taken:
+        print(item)
+
+
+testBest()
